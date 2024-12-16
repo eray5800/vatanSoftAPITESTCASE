7715abb493076ff901cfbc86b6be74fa21020b96
Eray Yenigün <erayfb7@hotmail.com>
2024-12-16 23:01:37 +0300

    Add project files.

---
+++ vatansoftAPITESTCASE/Controllers/DogAPIController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using vatansoftAPITESTCASE.HelperClasses;
+
+namespace vatansoftAPITESTCASE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DogAPIController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string _apiUrl;
+        private const int ByteLimit = 1050000;
+
+        public DogAPIController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _apiUrl = configuration["DogApiSettings:ApiUrl"];
+        }
+
+        [HttpGet("check-api-sizes")]
+        public async Task<IActionResult> CheckApiSizes()
+        {
+            int greaterCount = 0;
+            int smallerCount = 0;
+
+            var client = _httpClientFactory.CreateClient();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var response = await client.GetAsync(_apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    var apiResult = JsonSerializer.Deserialize<DogApiResponseDTO>(jsonString);
+
+                    if (apiResult.FileSizeBytes > ByteLimit)
+                    {
+                        greaterCount++;
+                    }
+                    else
+                    {
+                        smallerCount++;
+                    }
+                }
+            }
+
+            return Ok(new
+            {
+                GreaterThanLimit = greaterCount,
+                SmallerThanLimit = smallerCount
+            });
+        }
+    }
+}
---
+++ vatansoftAPITESTCASE/HelperClasses/DogApiResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace vatansoftAPITESTCASE.HelperClasses
+{
+    public class DogApiResponseDTO
+    {
+        [JsonPropertyName("fileSizeBytes")]
+        public int FileSizeBytes { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+
+}
